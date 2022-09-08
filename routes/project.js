469023170439d68f4effd60b5b1cf837dabcbdd9
TubesForLyfe <13520160@std.stei.itb.ac.id>
2022-09-08 21:33:10 +0700

    Deploy Profile API

--- routes/project.js
+++ routes/project.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Cache = require('node-cache');
-const Project = require('../models/Project');
+const Project = require('../models/project');
 
 const router = express.Router();
 const cache = new Cache({stdTTL: 10});
